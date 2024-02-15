8e15a407d0d625bed24c617de6ba7297e937beef
Sergio Santos <64509249+clowjs@users.noreply.github.com>
2024-02-15 10:45:25 -0300

    refactor: clean up

--- js/index.js
+++ js/index.js
@@ -1,18 +1,3 @@
-// const container = document.getElementsByClassName('container');
-
-// for (let i = 0; i < 16; i++) {
-//   const row = document.createElement('div');
-//   row.classList.add('row');
-
-//   for (let j = 0; j < 16; j++) {
-//     const square = document.createElement('div');
-//     square.classList.add('square');
-//     square.id = `${i + 1}x${j + 1}`;
-//     row.appendChild(square);
-//   }
-
-//   container[0].appendChild(row);
-// }
 
 const btnCreateGrid = document.getElementById('btn-create-grid');
 btnCreateGrid.addEventListener('click', createGrid);
